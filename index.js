feb4e01b3743f0561cd8bbf9fed1991e58bab327
Terry Mitchelle <mitchellterry307@gmail.com>
2023-10-01 12:51:48 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName= 'bob'
+const leastFavoriteCustomer='magebo'
+
+function upperCaseCustomerName(){
+    return customerName=customerName.toUpperCase()
+}
+function setBestCustomer(){
+    return bestCustomer= 'not bob'
+}
+function overwriteBestCustomer(newFavourite){
+    return bestCustomer = newFavourite
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'gati'
+  }
